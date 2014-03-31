3cc75a9707c73118e675064c3d7d347de282096e
at1as <jason@willems.ca>
2014-03-30 22:15:00 -0400

    Adding file upload to requests

--- rest.rb
+++ rest.rb
@@ -10,11 +10,11 @@ end
 
 get '/?' do
 	@requests = ["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"]
-	@timeout = ["1","2","3","4","5","10"]
+	@timeout = ["1","2","5","10","60"]
 	@validHeaderCount = 1
 	@headerHash = {"" => ""}
-	@follow, @verbose, @ssl = true, true, false
-	@visible = Array.new(5, "displayed")	#REQUEST, AUTH, HEADERS, PAYLOAD, RESULTS
+	@follow, @verbose, @ssl, @loggingOn = true, true, false, false
+	@visible = ["displayed", "hidden", "displayed", "displayed", "displayed"]	#Ordered display toggle for frontend: REQUEST, AUTH, HEADERS, PAYLOAD, RESULTS
 	erb :index
 end
 
@@ -30,7 +30,8 @@ post '/' do
 	@headerHash = {}
 	@validHeaderCount = 1
 	@formResponse = true
-	@follow, @verbose, @ssl = true, true, false
+	@follow, @verbose, @ssl, @loggingOn = true, true, false, false
+	@requestBody = ""
 	@visible = [params[:serveURLDiv], params[:serveAuthDiv], params[:serveHeaderDiv], params[:servePayloadDiv], params[:serveResultsDiv]]
 
 	(0..Integer(params[:headerCount])).each do |i|
@@ -43,6 +44,10 @@ post '/' do
 		end
 	end
 
+	if @headerHash["User-Agent"] == nil		#add branded User-Agent header, only if empty
+		@headerHash["User-Agent"] = "BetteR - https://github.com/at1as/BetteR"
+	end
+
 	if params[:followlocation] == ""
 		@follow = false
 	end
@@ -52,6 +57,14 @@ post '/' do
 	if params[:ssl_verifypeer] == "on"
 		@ssl = true
 	end
+	if params[:enableLogging] == "on"
+		@loggingOn = true
+	end
+	unless params[:datafile].nil?
+		@requestBody = { content: params[:payload], file: File.open(params[:datafile][:tempfile], 'r') }
+	else
+		@requestBody = { content: params[:payload] }
+	end
 
 	request = Typhoeus::Request.new(
 		params[:url],
@@ -59,8 +72,8 @@ post '/' do
 	  	:username => params[:usr],
 		:password => params[:pwd],
 		:auth_method => :auto,		#Should be manually set in order to test unideal APIs
-	  	body: params[:payload],
 	  	:headers => @headerHash,
+	  	:body => @requestBody,		#to replace -> body: params[:payload],
 	  	followlocation: @follow,
 	  	verbose: @verbose,
 	  	ssl_verifypeer: @ssl,
@@ -69,11 +82,18 @@ post '/' do
 
 	request.run
 	response = request.response
-	puts request.inspect 	#for debug only
 
-	#puts params[:serveHeaderDiv]
-	puts "!!!!"
-	puts @visible
+	if @headerHash["User-Agent"] == "BetteR - https://github.com/at1as/BetteR"
+		@headerHash.delete("User-Agent")
+	end
+
+	if @loggingOn == true
+		File.open('log/' + Time.now.strftime("%Y-%m-%d") + '.log', 'a') { 
+			|file| file.write("-"*10 + "\n" + Time.now.to_s + request.inspect + "\n\n" ) 
+		}
+	end
+
+	puts params[:payload]
 
 	@requestOptions = request.options
 	@returnBody = response.body 
@@ -82,7 +102,6 @@ post '/' do
 	@statCodeReturn = response.response_headers
 
 	@requests = ["#{params[:requestType]}", "GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"].uniq
-	@timeout = ["1","2","3","4","5","10"]
+	@timeout = ["1","2","5","10","60"]
 	erb :index
 end
-
