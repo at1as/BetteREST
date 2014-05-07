59ada7ed33ece4b7402cf816bd84eca95f5a34a0
at1as <jason@willems.ca>
2014-05-07 10:17:31 -0400

    More conformity with github ruby style guide

--- lib/better_rest.rb
+++ lib/better_rest.rb
@@ -30,6 +30,7 @@ get '/env' do
 end
 
 post '/' do
+	# Initalize
 	@headerHash = {}
 	@validHeaderCount = 1
 	@formResponse = true
@@ -37,20 +38,25 @@ post '/' do
 	@requestBody = ""
 	@visible = [params[:serveURLDiv], params[:serveAuthDiv], params[:serveHeaderDiv], params[:servePayloadDiv], params[:serveResultsDiv]]
 
+	# Loop through Header key/value pairs
 	(0..Integer(params[:headerCount])).each do |i|
 		@keyIncrement = "key" + "#{i}"
 		@valueIncrement = "value" + "#{i}"
 
+		# If a header is created and deleted before submit, headerCount will not renormalize (and will therefore exceed the number of sent headers)
+		# This will check that a particular header exists, before adding it
 		if params[@keyIncrement] && !params[@keyIncrement].empty? && params[@valueIncrement] && !params[@valueIncrement].empty?
 			@headerHash[params[@keyIncrement]] = params[@valueIncrement]
 			@validHeaderCount += 1
 		end
 	end
 
-	if @headerHash["User-Agent"] == nil		#add branded User-Agent header, only if empty
+	# Shameless branding. Only if User-Agent isn't user specified
+	if @headerHash["User-Agent"] == nil
 		@headerHash["User-Agent"] = "BetteR - https://github.com/at1as/BetteR"
 	end
 
+	# Check which options the user set
 	if params[:followlocation] == ""
 		@follow = false
 	end
@@ -63,49 +69,55 @@ post '/' do
 	if params[:enableLogging] == "on"
 		@loggingOn = true
 	end
-	unless params[:datafile].nil?
+	if !params[:datafile].nil?
 		@requestBody = { content: params[:payload], file: File.open(params[:datafile][:tempfile], 'r') }
 	else
 		@requestBody = { content: params[:payload] }
 	end
 
+	# Create the Request
 	request = Typhoeus::Request.new(
 		params[:url],
 	  	method: params[:requestType],
 	  	:username => params[:usr],
 		:password => params[:pwd],
-		:auth_method => :auto,		#Should be manually set in order to test unideal APIs
+		:auth_method => :auto,		#Should ideally not be auto, in order to test unideal APIs
 	  	:headers => @headerHash,
-	  	:body => @requestBody,		#to replace -> body: params[:payload],
+	  	:body => @requestBody,
 	  	followlocation: @follow,
 	  	verbose: @verbose,
 	  	ssl_verifypeer: @ssl,
 	  	:timeout => Integer(params[:timeoutInterval])
 	)
 
+	# Send the request
 	request.run
 	response = request.response
 
+	# If user-agent wasn't set by user, don't bother showing the user this default
 	if @headerHash["User-Agent"] == "BetteR - https://github.com/at1as/BetteR"
 		@headerHash.delete("User-Agent")
 	end
 
+	# Log the request response
 	if @loggingOn == true
 		File.open('log/' + Time.now.strftime("%Y-%m-%d") + '.log', 'a') { 
 			|file| file.write("-"*10 + "\n" + Time.now.to_s + request.inspect + "\n\n" ) 
 		}
 	end
 
+	# For Debug, prints to terminal. This can be supressed.
 	puts params[:payload]
 
+	# These values will be used by the ERB page
 	@requestOptions = request.options
 	@returnBody = response.body 
 	@returnCode = response.return_code
 	@returnTime = response.time
 	@statCodeReturn = response.response_headers
-
 	@requests = ["#{params[:requestType]}", "GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"].uniq
 	@timeout = ["1","2","5","10","60"]
+
 	erb :index
 end
 
