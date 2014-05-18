62b9416c461e0d3ed0421fa7a8c39c179c87efeb
Hassan Shamim <hassan.has.you@gmail.com>
2014-05-18 15:59:17 -0500

    eww hard tabs.  Replace with two spaces

--- lib/better_rest.rb
+++ lib/better_rest.rb
@@ -7,119 +7,114 @@ set :public_dir, File.expand_path('../../public', __FILE__)
 set :views, File.expand_path('../../views', __FILE__)
 
 helpers do
-	include Rack::Utils
-	alias_method :h, :escape_html
+  include Rack::Utils
+  alias_method :h, :escape_html
 end
 
+BETTER_SIGNATURE = "BetteR - https://github.com/at1as/BetteR"
+
 get '/?' do
-	@requests = ["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"]
-	@timeout = ["1","2","5","10","60"]
-	@validHeaderCount = 1
-	@headerHash = {"" => ""}
-	@follow, @verbose, @ssl, @loggingOn = true, true, false, false
-	@visible = ["displayed", "hidden", "displayed", "displayed", "displayed"]	#Ordered display toggle for frontend: REQUEST, AUTH, HEADERS, PAYLOAD, RESULTS
-	erb :index
+  @requests = ["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"]
+  @timeout = ["1","2","5","10","60"]
+  @validHeaderCount = 1
+  @headerHash = {"" => ""}
+  @follow, @verbose, @ssl, @loggingOn = true, true, false, false
+  @visible = ["displayed", "hidden", "displayed", "displayed", "displayed"]	#Ordered display toggle for frontend: REQUEST, AUTH, HEADERS, PAYLOAD, RESULTS
+  erb :index
 end
 
 get '/env' do
-	<<-ENDRESPONSE
-    	Ruby:    #{RUBY_VERSION} <br/>
-    	Rack:    #{Rack::VERSION} <br/>
-    	Sinatra: #{Sinatra::VERSION}
-  	ENDRESPONSE
+  <<-ENDRESPONSE
+      Ruby:    #{RUBY_VERSION} <br/>
+      Rack:    #{Rack::VERSION} <br/>
+      Sinatra: #{Sinatra::VERSION}
+  ENDRESPONSE
 end
 
 post '/' do
-	# Initalize
-	@headerHash = {}
-	@validHeaderCount = 1
-	@formResponse = true
-	@follow, @verbose, @ssl, @loggingOn = true, true, false, false
-	@requestBody = ""
-        @visible = [:servURLDiv, :servAuthDiv, :servHeaderDiv, :servePayloadDiv, :servResultsDiv].map{ |k| params[k] }
+  # Initalize
+  @headerHash = {}
+  @validHeaderCount = 1
+  @formResponse = true
+  @follow, @verbose, @ssl, @loggingOn = true, true, false, false
+  @requestBody = ""
+  @visible = [:servURLDiv, :servAuthDiv, :servHeaderDiv, :servePayloadDiv, :servResultsDiv].map{ |k| params[k] }
 
-	# Loop through Header key/value pairs
-	params[:headerCount].to_i.times do |i|
-		@keyIncrement = "key#{i}"
-		@valueIncrement = "value#{i}"
+  # Loop through Header key/value pairs
+  params[:headerCount].to_i.times do |i|
+    @keyIncrement = "key#{i}"
+    @valueIncrement = "value#{i}"
 
-		# If a header is created and deleted before submit, headerCount will not renormalize (and will therefore exceed the number of sent headers)
-		# This will check that a particular header exists, before adding it
-		if !(params.fetch(@keyIncrement, '').empty? || params.fetch(@valueIncrement, '').empty?)
-			@headerHash[params[@keyIncrement]] = params[@valueIncrement]
-			@validHeaderCount += 1
-		end
-	end
+  # If a header is created and deleted before submit, headerCount will not renormalize (and will therefore exceed the number of sent headers)
+  # This will check that a particular header exists, before adding it
+    if !(params.fetch(@keyIncrement, '').empty? || params.fetch(@valueIncrement, '').empty?)
+      @headerHash[params[@keyIncrement]] = params[@valueIncrement]
+      @validHeaderCount += 1
+    end
+  end
 
-	# Shameless branding. Only if User-Agent isn't user specified
-	@headerHash["User-Agent"] ||= "BetteR - https://github.com/at1as/BetteR"
+  # Shameless branding. Only if User-Agent isn't user specified
+  @headerHash["User-Agent"] ||= BETTER_SIGNATURE
 
-	# Check which options the user set
-	if params[:followlocation] == ""
-		@follow = false
-	end
-	if params[:verbose] == ""
-		@verbose = false
-	end
-	if params[:ssl_verifypeer] == "on"
-		@ssl = true
-	end
-	if params[:enableLogging] == "on"
-		@loggingOn = true
-	end
+  # Check which options the user set
+  @follow = false if params[:followlocation] == ""
 
-	if params[:datafile]
-		@requestBody = { content: params[:payload], file: File.open(params[:datafile][:tempfile], 'r') }
-	else
-		@requestBody = { content: params[:payload] }
-	end
+  @verbose = false if params[:verbose] == ""
 
-	# Create the Request
-	request = Typhoeus::Request.new(
-		params[:url],
-	  	method: params[:requestType],
-	  	:username => params[:usr],
-		:password => params[:pwd],
-		:auth_method => :auto,		#Should ideally not be auto, in order to test unideal APIs
-	  	:headers => @headerHash,
-	  	:body => @requestBody,
-	  	followlocation: @follow,
-	  	verbose: @verbose,
-	  	ssl_verifypeer: @ssl,
-	  	:timeout => Integer(params[:timeoutInterval])
-	)
+  @ssl = true if params[:ssl_verifypeer] == "on"
 
-	# Send the request
-	request.run
-	response = request.response
+  @loggingOn = true if params[:enableLogging] == "on"
 
-	# If user-agent wasn't set by user, don't bother showing the user this default
-	if @headerHash["User-Agent"] == "BetteR - https://github.com/at1as/BetteR"
-		@headerHash.delete("User-Agent")
-	end
+  if params[:datafile]
+    @requestBody = { content: params[:payload], file: File.open(params[:datafile][:tempfile], 'r') }
+  else
+    @requestBody = { content: params[:payload] }
+  end
 
-	# Log the request response
-	if @loggingOn
-		File.open('log/' + Time.now.strftime("%Y-%m-%d") + '.log', 'a') do |file|
-			file.write("-"*10 + "\n" + Time.now.to_s + request.inspect + "\n\n" )
-		end
-	end
+  # Create the Request
+  request = Typhoeus::Request.new(
+    params[:url],
+    method: params[:requestType],
+    username: params[:usr],
+    password: params[:pwd],
+    auth_method: :auto,		#Should ideally not be auto, in order to test unideal APIs
+    headers: @headerHash,
+    body: @requestBody,
+    followlocation: @follow,
+    verbose: @verbose,
+    ssl_verifypeer: @ssl,
+    timeout: Integer(params[:timeoutInterval])
+  )
 
-	# For Debug, prints to terminal. This can be supressed.
-	puts params[:payload]
+  # Send the request
+  request.run
+  response = request.response
 
-	# These values will be used by the ERB page
-	@requestOptions = request.options
-	@returnBody = response.body 
-	@returnCode = response.return_code
-	@returnTime = response.time
-	@statCodeReturn = response.response_headers
-	@requests = ["#{params[:requestType]}", "GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"].uniq
-	@timeout = ["1","2","5","10","60"]
+  # If user-agent wasn't set by user, don't bother showing the user this default
+  @headerHash.delete("User-Agent") if @headerHash["User-Agent"] == BETTER_SIGNATURE
 
-	erb :index
+  # Log the request response
+  if @loggingOn
+    File.open('log/' + Time.now.strftime("%Y-%m-%d") + '.log', 'a') do |file|
+      file.write("-" * 10 + "\n" + Time.now.to_s + request.inspect + "\n\n" )
+    end
+  end
+
+  # For Debug, prints to terminal. This can be supressed.
+  puts params[:payload]
+
+  # These values will be used by the ERB page
+  @requestOptions = request.options
+  @returnBody = response.body
+  @returnCode = response.return_code
+  @returnTime = response.time
+  @statCodeReturn = response.response_headers
+  @requests = ["#{params[:requestType]}", "GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"].uniq
+  @timeout = ["1","2","5","10","60"]
+
+  erb :index
 end
 
 not_found do
-	redirect '/'
+  redirect '/'
 end
